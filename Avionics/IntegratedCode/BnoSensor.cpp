3b6aa3a22174d7724206946da2c6736d43f790ff
Wanderer <moommala.alexander@gmail.com>
2026-04-24 16:30:12 -0700

    Added linear accel, angular accel, and rawaccel

--- Avionics/IntegratedCode/BnoSensor.cpp
+++ Avionics/IntegratedCode/BnoSensor.cpp
@@ -32,12 +32,35 @@ bool BnoSensor::loadCalibration() { // Opens flash memory to load stored calibra
 }
 
 bool BnoSensor::update(){
+  //events
   sensors_event_t event;
   myBno.getEvent(&event);
+
+  //Acceleration Vector
+  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+  //Gyroscope Vector
+  imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+  //Raw acceleration
+  imu::Vector<3> rawAccel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+
+  dt = (currentTime - lastTime) / 1000.0;
 
   myOX = event.orientation.x, 4;
   myOY = event.orientation.y, 4;
   myOZ = event.orientation.z, 4;
+
+  ax = accel.x();
+  ay = accel.y();
+  az = accel.z();
+
+  gx = gyro.x();
+  gy = gyro.y();
+  gz = gyro.z();
+
+  float rawAccelMag = sqrt(rawAccel.x() * rawAccel.x() + 
+  rawAccel.y() * rawAccel.y() + rawAccel.z() * rawAccel.z());
 
   return true;
 }
